74b444ac431d2c0c3e853d369e27ec3464bbd6d7
Aldi Kurniawan <aldik889@gmail.com>
2020-11-01 20:08:19 +0700

    add box shadow pada logo

--- panel/authentication/login.php
+++ panel/authentication/login.php
@@ -30,6 +30,7 @@
             border-radius: 50%;
             background-color: white;
             display: flex;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         }
 
         .logo-desa img {
