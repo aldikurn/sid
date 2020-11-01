cd7e36a5b8d541137cc287c9d8c179f615cbcb0b
Aldi Kurniawan <aldik889@gmail.com>
2020-11-01 20:08:50 +0700

    add activity history

--- panel/history-aktifitas/history-aktifitas.php
+++ panel/history-aktifitas/history-aktifitas.php
@@ -1,1 +1,60 @@
-<?php
+<div class="row">
+    <div class="col-md-12">
+        <!-- The time line -->
+        <div class="timeline">
+            <!-- timeline time label -->
+            <div class="time-label">
+                <span class="bg-red">01 Oct 2020</span>
+            </div>
+            <!-- /.timeline-label -->
+            <!-- timeline item -->
+
+            <!-- END timeline item -->
+            <!-- timeline item -->
+            <div>
+                <i class="fas fa-user-tie bg-blue"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fas fa-clock"></i> 5 mins ago</span>
+                    <h3 class="timeline-header no-border"><a href="#">Admin</a> Menambahkan data penduduk</h3>
+                    <div class="timeline-body">
+                       lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+                    </div>
+                </div>
+            </div>
+            <!-- END timeline item -->
+            <!-- timeline item -->
+            <div>
+                <i class="fas fa-user-tie bg-blue"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fas fa-clock"></i> 27 mins ago</span>
+                    <h3 class="timeline-header"><a href="#">Admin</a> Mengubah data penduduk</h3>
+                    <div class="timeline-body">lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet</div>
+
+                </div>
+            </div>
+            <!-- END timeline item -->
+            <!-- timeline time label -->
+            <div class="time-label">
+                <span class="bg-green">3 Jan. 2014</span>
+            </div>
+            <!-- /.timeline-label -->
+            <!-- timeline item -->
+            <div>
+                <i class="fa fa-user-tie bg-blue"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fas fa-clock"></i> 2 days ago</span>
+                    <h3 class="timeline-header"><a href="#">Admin</a> Menambah mengubah data penduduk</h3>
+                    <div class="timeline-body">
+                        lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+                    </div>
+                </div>
+            </div>
+            <!-- END timeline item -->
+            <!-- timeline item -->
+
+            <!-- END timeline item -->
+
+        </div>
+    </div>
+    <!-- /.col -->
+</div><?php
