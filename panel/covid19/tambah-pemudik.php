f110fba17831207fa92bd05e2ffb26c6e2002548
Aldi Kurniawan <aldik889@gmail.com>
2020-11-08 15:39:53 +0700

    add pemudik service

--- panel/covid19/tambah-pemudik.php
+++ panel/covid19/tambah-pemudik.php
@@ -1,32 +1,98 @@
-
+<style>
+    #fg-nik {
+        display: grid;
+        align-items: end;
+        gap: 0.7em;
+        grid-template-columns: 1fr 100px;
+    }
+</style>
 <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <ul class="nav nav-tabs" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab" href="#domisili">Penduduk Domisili</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#nondomisili">Penduduk Non Domisili</a>
-                    </li>
-                </ul>
+                <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <a class="nav-item nav-link active" id="nav-domisili-tab" data-toggle="tab" href="#nav-domisili" role="tab" aria-controls="nav-home" aria-selected="true">Domisili</a>
+                        <a class="nav-item nav-link" id="nav-nondomisili-tab" data-toggle="tab" href="#nav-nondomisili" role="tab" aria-controls="nav-profile" aria-selected="false">Non Domisili</a>
+                    </div>
+                </nav>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="tab-content">
-                            <div id="domisili" class="container tab-pane active"><br>
-                                <h3>Domisili</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div class="tab-content" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="nav-domisili" role="tabpanel" aria-labelledby="nav-home-tab">
+                                <form id="tambahpemudik" action="" method="POST">
+                                    <div class="form-group" id="fg-nik">
+                                        <div>
+                                            <label>NIK <em style="font-weight: normal">(lihat menu daftar penduduk untuk melihat daftar nik)</em></label>
+                                            <input type="text" class="form-control" placeholder="NIK" name="nik" required>
+                                        </div>
+                                        <a class="btn btn-default" id="cekdata">Cek Data</a>
+                                    </div>
+                                    <div class="form-group" id="fg-nama">
+                                        <label>Nama</label>
+                                        <input type="text" class="form-control"  name="nama" value="---" disabled required>
+                                    </div>
+                                    <div class="form-group" id="fg-ttl">
+                                        <label>Tempat tanggal lahir</label>
+                                        <input type="text" class="form-control" name="ttl" value="---" disabled required>
+                                    </div>
+                                    <div class="form-group" id="fg-alamat">
+                                        <label>Alamat</label>
+                                        <input type="text" class="form-control" name="alamat" value="---" disabled required>
+                                    </div>
+                                    <div class="form-group" id="fg-asal-pemudik">
+                                        <label>Kota Asal Pemudik</label>
+                                        <input type="text" class="form-control" placeholder="Asal Pemudik" name="asal" required>
+                                    </div>
+                                    <div class="form-group" id="fg-tujuan">
+                                        <label>Tujuan Pemudik</label>
+                                        <select  class="form-control" name="tujuan" required>
+                                            <option value="" selected disabled>Pilih Tujuan Pemudik</option>
+                                            <option value="liburan">Liburan</option>
+                                            <option value="menjenguk keluarga">Menjenguk Keluarga</option>
+                                            <option value="pulang kampung">Pulang Kampung</option>
+                                            <option value="lainnya">Lainnya</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group" id="fg-durasi">
+                                        <label>Durasi Mudik (Hari)</label>
+                                        <input type="number" class="form-control" name="durasi" value="0" required>
+                                    </div>
+                                    <div class="form-group" id="fg-status-covid19">
+                                        <label>Status COVID19</label>
+                                        <select  class="form-control" name="status-covid19" required>
+                                            <option value="" selected disabled>Pilih Status COVID19</option>
+                                            <option value="positif">Positif</option>
+                                            <option value="pdp">Pasien Dalam Pengawasan (PDP)</option>
+                                            <option value="odp">Orang Dalam Pemantauan (ODP)</option>
+                                            <option value="odr">Orang Dalam Resiko (ODR)</option>
+                                            <option value="otg">Orang Tanpa Gejala (OTG)</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group" id="fg-wajib-pantau">
+                                        <label>Wajib Pantau</label>
+                                        <select  class="form-control" name="wajib-pantau" required>
+                                            <option value="ya">Ya</option>
+                                            <option value="tidak">Tidak</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group" id="fg-keluhan-kesehatan">
+                                        <label>Keluhan Kesehatan</label>
+                                        <textarea class="form-control" name="keluhan-kesehatan" id="keluhan-kesehatan" rows="3" ></textarea>
+                                    </div>
+                                    <button type="submit" name="submit" class="btn btn-success"><i class="fas fa-save"></i> Simpan</button>
+                                </form>
                             </div>
-                            <div id="nondomisili" class="container tab-pane fade"><br>
-                                <h3>Non Domisili</h3>
-                                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <div class="tab-pane fade" id="nav-nondomisili" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                <p>Untuk menambahkan data penduduk non domisili, silahkan masuk ke menu daftar penduduk</p>
+                                    <form action="" method="POST">
+                                        <button type="submit" name="path" value="kependudukan/daftar-penduduk.php" class="btn btn-default">Pergi</button>
+                                    </form>
                             </div>
                         </div>
-                        <!-- /.chart-responsive -->
                     </div>
                     <!-- /.col -->
                 </div>
@@ -38,3 +104,92 @@
     </div>
     <!-- /.col -->
 </div>
+<script>
+    let form = document.forms['tambahpemudik'];
+    let dataLengkap = false;
+    document.getElementById('cekdata').addEventListener('click', function(event) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '<?= $index_location ?>/services/penduduk.php?action=select&nik=' + form['nik'].value);
+        xhr.onload = function() {
+            if(xhr.status == 200) {
+                try {
+                    let response = JSON.parse(xhr.responseText);
+                    if(response.code === 0) {
+                        form['nama'].value = response.data.nama;
+                        form['ttl'].value = response.data.tempat_lahir + ", " + response.data.tanggal_lahir;
+                        form['alamat'].value = 'Dusun ' + response.data.nama_dusun + ' RW ' + response.data.nomor_rw + ' RT ' + response.data.nomor_rt;
+                        dataLengkap = true;
+                        toastr.success("Data ditemukan")
+                    } else {
+                        toastr.error("Data tidak ditemukan")
+                        console.log(xhr.responseText);
+                        console.log(e.message);
+                    }
+                } catch (e) {
+                    toastr.error("Terjadi Kesalahan")
+                    console.log(xhr.responseText);
+                    console.log(e.message);
+                }
+            } else {
+                toastr.error("Kesalahan sambungan ke server")
+                console.log(xhr.responseText);
+                console.log(e.message);
+            }
+        };
+        xhr.send();
+    });
+
+    form['nik'].addEventListener('keypress', function(event) {
+       form['nama'].value = '---';
+       form['ttl'].value = '---';
+       form['alamat'].value = '---';
+       dataLengkap = false;
+    });
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        if(dataLengkap) {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', '<?= $index_location ?>/services/pemudik.php?action=insert&nik=' + form['nik'].value);
+            xhr.onload = function() {
+                if(xhr.status === 200) {
+                    try {
+                        let response = JSON.parse(xhr.responseText);
+                        if(response.code === 0) {
+                            resetForm();
+                            toastr.success('Berhasil menambahkan data pemudik');
+                        } else {
+                            toastr.error("Terjadi Kesalahan")
+                            console.log(xhr.responseText);
+                        }
+                    } catch(e) {
+                        toastr.error("Terjadi Kesalahan")
+                        console.log(xhr.responseText);
+                        console.log(e.message);
+                    }
+                } else {
+                    toastr.error("Terjadi kesalaahan saat menghubungi server")
+                    console.log(xhr.responseText);
+                    console.log(e.message);
+                }
+            }
+            let formData = new FormData(form);
+            xhr.send(formData);
+        } else {
+            toastr.warning('Data tidak lengkap');
+        }
+    });
+
+    function resetForm() {
+        form['nik'].value = '';
+        form['nama'].value = '---';
+        form['ttl'].value = '---';
+        form['alamat'].value = '---';
+        form['asal'].value = '';
+        form['tujuan'].selectedIndex = 0;
+        form['durasi'].value = 0;
+        form['status-covid19'].selectedIndex = 0;
+        form['wajib-pantau'].selectedIndex = 0;
+        form['keluhan-kesehatan'].value = '';
+    }
+</script>
---
+++ panel/services/pemudik.php
@@ -0,0 +1,86 @@
+<?php
+include_once('DatabaseHelper.php');
+$response['code'] = -1;
+$response['message'] = "Terjadi kesalahan";
+$response['data'] = null;
+
+if (isset($_GET['action']) && isset($_GET['nik'])) {
+
+    if ($_GET['action'] === 'select') {
+        if ($_GET['nik'] == 'all') {
+            $stmt = $conn->prepare("SELECT * FROM pemudik");
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $response['data'] = array();
+            foreach ($result as $row) {
+                $response['data'][] = $row;
+            }
+
+            $stmt->close();
+            $response['code'] = 0;
+            $response['message'] = "Berhasil mengambil semua records";;
+        } else {
+            $stmt = $conn->prepare('SELECT pemudik.*, penduduk.nama FROM pemudik INNER JOIN penduduk ON pemudik.nik = penduduk.nik WHERE pemudik.nik = ?');
+            $stmt->bind_param("s", $_GET['nik']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                foreach ($result as $row) {
+                    $response['data'] = $row;
+                }
+
+                $response['code'] = 0;
+                $response['message'] = "Berhasil mengambil record penduduk berdasarkan nik";
+            } else {
+                $response['code'] = 1;
+                $response['message'] = "Data tidak ditemukan";
+            }
+            $stmt->close();
+        };
+    } elseif ($_GET['action'] === 'insert') {
+        $dataLengkap = false;
+        $data = array();
+
+        $data['nik'] = $_POST['nik'];
+        $data['asal'] = $_POST['asal'];
+        $data['tujuan'] = $_POST['tujuan'];
+        $data['durasi'] = $_POST['durasi'];
+        $data['status_covid19'] = $_POST['status-covid19'];
+        $data['wajib_pantau'] = $_POST['wajib-pantau'];
+        $data['keluhan_kesehatan'] = $_POST['keluhan-kesehatan'];
+
+
+        foreach ($data as $val) {
+            if (!isset($val)) {
+                $dataLengkap = false;
+            } else {
+                $dataLengkap = true;
+            }
+        }
+
+        if ($dataLengkap) {
+
+            $stmt = $conn->prepare("INSERT INTO pemudik(nik, asal, tujuan, durasi, status_covid19, wajib_pantau, keluhan_kesehatan) VALUES(?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssssss", $data['nik'], $data['asal'], $data['tujuan'], $data['durasi'], $data['status_covid19'], $data['wajib_pantau'], $data['keluhan_kesehatan']);
+            $stmt->execute();
+            $stmt->close();
+
+            $response['code'] = 0;
+            $response['message'] = "Data berhasil dimasukkan";
+        } else {
+            $response['code'] = -1;
+            $response['message'] = "Data tidak lengkap";
+        }
+    } elseif ($_GET['action'] === 'delete') {
+        $stmt = $conn->prepare("DELETE FROM pemudik WHERE nik = ?");
+        $stmt->bind_param("s", $_GET['nik']);
+        $stmt->execute();
+        $stmt->close();
+        $response['code'] = 0;
+        $response['message'] = "Data penduduk berhasil dihapus berdasarkan nik";
+    } else {
+        $response['message'] = "Parameter tidak valid";
+    }
+}
+echo json_encode($response);
