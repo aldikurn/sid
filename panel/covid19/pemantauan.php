0316172827705e3a3e58295a700e5406c6f9b15a
Aldi Kurniawan <aldik889@gmail.com>
2020-11-09 10:54:20 +0700

    add tambah data pantau pemudik

---
+++ panel/covid19/pemantauan.php
@@ -0,0 +1,284 @@
+<style>
+    #formTambahDataPemantauan {
+        display: grid;
+        gap: 1rem;
+        grid-template-areas:
+            'fg-nik  fg-tanggal-pemantauan'
+            'fg-tanggal-tiba fg-data-hari-ke'
+            'fg-suhu-tubuh fg-kondisi'
+            'fg-keluhan-lain fg-keluhan-lain'
+            'fg-submit fg-submit';
+    }
+    
+    #fg-nik {
+        grid-area: fg-nik;
+    }
+
+    #fg-tanggal-pemantauan {
+        grid-area: fg-tanggal-pemantauan;
+    }
+
+    #fg-tanggal-tiba {
+        grid-area: fg-tanggal-tiba;
+    }
+
+    #fg-data-hari-ke {
+        grid-area: fg-data-hari-ke;
+    }
+
+    #fg-suhu-tubuh {
+        grid-area: fg-suhu-tubuh;
+    }
+
+    #fg-kondisi {
+        grid-area: fg-kondisi;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    #fg-kondisi > div .form-check {
+        display: inline-block;
+        margin-right: 0.3rem;
+    }
+
+    #fg-keluhan-lain {
+        grid-area: fg-keluhan-lain;
+    }
+
+    #fg-submit {
+        grid-area: fg-submit;
+    }
+
+    .card-header {
+        display: flex;
+        justify-content: space-between;
+    }
+
+</style>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card collapsed-card">
+            <div class="card-header">
+                <h5 style="margin-right: auto">Tambah data pemantauan</h5>
+                <div>
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-plus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <form id="formTambahDataPemantauan" method="post" action="">
+                    <div class="form-group" id="fg-nik">
+                        <label>Pilih Pemudik</label>
+                        <select name="nik" id="nik" class="form-control" required>
+                            <option value="">Pilih Pemudik</option>
+                        </select>
+                    </div>
+                    <div class="form-group" id="fg-tanggal-pemantauan">
+                        <label>Tanggal Pemantauan</label>
+                        <input type="datetime-local" name="tanggal_pemantauan" class="form-control" required>
+                    </div>
+                    <div class="form-group" id="fg-tanggal-tiba">
+                        <label>Tanggal Tiba</label>
+                        <input type="text" name="tanggal_tiba" class="form-control" disabled>
+                    </div>
+                    <div class="form-group" id="fg-data-hari-ke">
+                        <label>Data Hari ke-</label>
+                        <input type="text" name="data-hari-ke" class="form-control" disabled>
+                    </div>
+                    <div class="form-group" id="fg-suhu-tubuh">
+                        <label>Suhu Tubuh</label>
+                        <input type="number" name="suhu-tubuh" class="form-control" placeholder="36" min="20" max="50" required>
+                    </div>
+                    <div class="form-group" id="fg-kondisi">
+                        <label>Kondisi Kesehatan</label>
+                        <div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="ya" id="batuk" name="batuk">
+                                <label class="form-check-label" for="batuk">Batuk</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="ya" id="flu" name="flu">
+                                <label class="form-check-label" for="flu">Flu</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="ya" id="sesak-nafas" name="sesak-nafas">
+                                <label class="form-check-label" for="sesak-nafas">Sesak Nafas</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group" id="fg-keluhan-lain">
+                        <label>Keluhan Lain</label>
+                        <textarea name="keluhan-lain" id="keluhan-lain" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group" id="fg-submit">
+                        <button type="submit" class="btn btn-success"><i class="fas fa-save"></i> Simpan</button>
+                    </div>
+                </form>
+            </div>
+            <!-- ./card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.col -->
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h5>Daftar Pemantauan</h5>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12">
+
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- ./card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.col -->
+</div>
+<script>
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '<?= $index_location ?>/services/pemudik.php?action=select&nik=all&wajib_pantau=1');
+    xhr.onload = function() {
+        if(xhr.status === 200) {
+            try {
+                let response = JSON.parse(xhr.responseText);
+
+                if(response.code === 0) {
+                    const len = response.data.length;
+                    let nikSelect = document.getElementsByName('nik')[0];
+                    for(i = 0; i < len; i++) {
+                        let opt = document.createElement('option');
+                        opt.value = response.data[i].nik;
+                        opt.textContent = response.data[i].nik + ' - ' + response.data[i].nama;
+                        nikSelect.appendChild(opt);
+                    }
+                } else {
+                    toastr.error('Terjadi Kesalahan');
+                    console.log(xhr.responseText);
+                }
+            } catch (e) {
+                toastr.error('Terjadi kesalahan');
+                console.log(e.message);
+                console.log(xhr.responseText);
+
+            }
+        } else {
+            toastr.error('Terjadi kesalahan saat menghubungi server');
+            console.log(xhr.responseText);
+        }
+    }
+    xhr.send();
+
+    let form = document.forms['formTambahDataPemantauan'];
+    form['nik'].addEventListener('change', function(event) {
+        if(this.value !== '') {
+            let ajax = new XMLHttpRequest();
+            ajax.open('GET', '<?= $index_location ?>/services/pemudik.php?action=select&nik=' + this.value);
+            ajax.onload = function() {
+                if(ajax.status === 200) {
+                    try {
+                        let response = JSON.parse(ajax.responseText);
+
+                        if(response.code === 0) {
+                            form['tanggal_tiba'].value = response.data.tanggal_tiba;
+                            form['tanggal_pemantauan'].value = formatDate(response.data.tanggal_tiba);
+                            form['tanggal_pemantauan'].setAttribute('min', formatDate(response.data.tanggal_tiba));
+                            form['tanggal_pemantauan'].setAttribute('max', formatDate(new Date(formatDate(response.data.tanggal_tiba)).getTime() + (24 * 3600 * 1000 * response.data.durasi)));
+                            changeTanggalPemantauan();
+                        } else {
+                            toastr.error('Terjadi Kesalahan');
+                            console.log(ajax.responseText);
+                        }
+                    } catch (e) {
+                        toastr.error('Terjadi kesalahan');
+                        console.log(e.message);
+                        console.log(ajax.responseText);
+
+                    }
+                } else {
+                    toastr.error('Terjadi kesalahan saat menghubungi server');
+                    console.log(ajax.responseText);
+                }
+            };
+        ajax.send();
+        } else {
+            form.reset();
+        }
+    });
+
+    form['tanggal_pemantauan'].addEventListener('change', function() {
+        changeTanggalPemantauan();
+    });
+
+    function changeTanggalPemantauan() {
+        let d1 = new Date(form['tanggal_pemantauan'].value);
+        let d2 = new Date(form['tanggal_tiba'].value);
+        form['data-hari-ke'].value = Math.round(new Date(d1 - d2).getTime() / (24 * 3600 * 1000));
+    }
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const ajax = new XMLHttpRequest();
+        ajax.open('POST', '<?= $index_location ?>/services/pantau_pemudik.php?action=insert&nik=' + form['nik'].value);
+        ajax.onload = function() {
+            if(xhr.status === 200) {
+                try {
+                    let response = JSON.parse(ajax.responseText);
+
+                    if(response.code === 0) {
+                        toastr.success(response.message);
+                        form.reset();
+                    } else {
+                        toastr.error('Terjadi Kesalahan');
+                        console.log(ajax.responseText);
+                    }
+                } catch (e) {
+                    toastr.error('Terjadi kesalahan');
+                    console.log(e.message);
+                    console.log(ajax.responseText);
+                }
+            } else {
+                toastr.error('Terjadi kesalahan saat menghubungi server');
+                console.log(ajax.responseText);
+            }
+        };
+        const formData = new FormData(form);
+        ajax.send(formData);
+    });
+
+    function formatDate(date) {
+        let d = new Date(date);
+        let month = d.getMonth() + 1;
+        let day = d.getDate();
+        let year = d.getFullYear();
+        let hour = d.getHours();
+        let minute = d.getMinutes();
+
+        if (month < 10) 
+            month = '0' + month;
+        if (day < 10) 
+            day = '0' + day;
+        if(hour < 10)
+            hour = '0' + hour;
+        if(minute < 10)
+            minute = '0' + minute;
+        let result = [year, month, day].join('-');
+        result = result + 'T' + hour + ':' + minute;
+        return result;
+    }
+</script>
---
+++ panel/services/pantau_pemudik.php
@@ -0,0 +1,96 @@
+<?php
+include_once('DatabaseHelper.php');
+$response['code'] = -1;
+$response['message'] = "Terjadi kesalahan";
+$response['data'] = null;
+
+if (isset($_GET['action']) && isset($_GET['nik'])) {
+
+    if ($_GET['action'] === 'select') {
+        if ($_GET['nik'] == 'all') {
+            $stmt = $conn->prepare("SELECT pantau_pemudik.*, penduduk.nama, pemudik.status_covid19 FROM pantau_pemudik INNER JOIN penduduk ON penduduk.nik = pantau_pemudik.nik INNER JOIN pemudik ON pemudik.nik = pantau_pemudik.nik");
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $response['data'] = array();
+            foreach ($result as $row) {
+                $response['data'][] = $row;
+            }
+
+            $response['message'] = "Berhasil mengambil semua records";
+            $stmt->close();
+            $response['code'] = 0;
+        } else {
+            $stmt = $conn->prepare('SELECT pantau_pemudik.*, penduduk.nama, pemudik.status_covid19 FROM pantau_pemudik INNER JOIN penduduk ON penduduk.nik = pantau_pemudik.nik INNER JOIN pemudik ON pemudik.nik = pantau_pemudik.nik WHERE pemudik.nik = ?');
+            $stmt->bind_param("s", $_GET['nik']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                foreach ($result as $row) {
+                    $response['data'] = $row;
+                }
+
+                $response['code'] = 0;
+                $response['message'] = "Berhasil mengambil record penduduk berdasarkan nik";
+            } else {
+                $response['code'] = 1;
+                $response['message'] = "Data tidak ditemukan";
+            }
+            $stmt->close();
+        };
+    } elseif ($_GET['action'] === 'insert') {
+        $dataLengkap = false;
+        $data = array();
+
+        $data['nik'] = $_POST['nik'];
+        $data['waktu_pantau'] = $_POST['tanggal_pemantauan'];
+        $data['suhu_tubuh'] = $_POST['suhu-tubuh'];
+        if(isset($_POST['batuk'])) {
+            $data['batuk'] = 'ya';
+        } else {
+            $data['batuk'] ='tidak';
+        }
+        if(isset($_POST['flu'])) {
+            $data['flu'] = 'ya';
+        } else {
+            $data['flu'] = 'tidak';
+        }
+        if(isset($_POST['sesak-nafas'])) {
+            $data['sesak_nafas'] = 'ya';
+        } else {
+            $data['sesak_nafas'] = 'tidak';
+        }
+        $data['keluhan_lain'] = $_POST['keluhan-lain'];
+
+
+        foreach ($data as $val) {
+            if (!isset($val)) {
+                $dataLengkap = false;
+            } else {
+                $dataLengkap = true;
+            }
+        }
+        if ($dataLengkap) {
+            $stmt = $conn->prepare("INSERT INTO pantau_pemudik(nik, waktu_pantau, suhu_tubuh, batuk, flu, sesak_nafas, keluhan_lain) VALUES(?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("ssissss", $data['nik'], $data['waktu_pantau'], $data['suhu_tubuh'], $data['batuk'], $data['flu'], $data['sesak_nafas'], $data['keluhan_lain']);
+            $stmt->execute();
+            $stmt->close();
+
+            $response['code'] = 0;
+            $response['message'] = "Data berhasil dimasukkan";
+        } else {
+            $response['code'] = -1;
+            $response['message'] = "Data tidak lengkap";
+        }
+    } elseif ($_GET['action'] === 'delete') {
+        $stmt = $conn->prepare("DELETE FROM pemudik WHERE nik = ?");
+        $stmt->bind_param("s", $_GET['nik']);
+        $stmt->execute();
+        $stmt->close();
+        $response['code'] = 0;
+        $response['message'] = "Data penduduk berhasil dihapus berdasarkan nik";
+    } else {
+        $response['message'] = "Parameter tidak valid";
+    }
+}
+echo json_encode($response);
